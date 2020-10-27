6719257f94f1ac9469bf2666dc382816fe26104b
Tony <tony.panczenko@gmail.com>
2020-10-27 22:42:01 +0000

    change styles, add grid, add lightbox, style lightbox, add event handlers for lightbox

--- js/app.js
+++ js/app.js
@@ -9,6 +9,10 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
+  /* ============================================= */
+  /*                   Declarations                */
+  /* ============================================= */
+
   function checkHTTPstatus(response) {
     if (response.status === 200) {
       return Promise.resolve(response);
@@ -33,12 +37,11 @@ document.addEventListener("DOMContentLoaded", () => {
       promise = promise.catch(attempt);
     }
 
-    promise.catch(reason => {
+    return promise.catch(reason => {
       console.log("Error when fetching data:", reason);
       // keep promise chain rejected on return
       throw reason;
     });
-    return promise;
   }
 
   function displayDirectory(responseJSON) {
@@ -52,11 +55,68 @@ document.addEventListener("DOMContentLoaded", () => {
       ulHTML += `<p class='city-location'>${el.location.city}</p></div>`;
       ulHTML += "</li>";
     });
-    const directoryList = document.querySelector("ul.directory-list");
     directoryList.innerHTML = ulHTML;
+    return responseJSON;
   }
 
+  function saveJSON(responseJSON) {
+    employeesJSON = responseJSON.results;
+    return responseJSON;
+  }
+
+  function findEmployeeByEmail(arr, email) {
+    return arr.filter(el => el.email === email)[0];
+  }
+
+  function createModal(e) {
+    if (e.target.classList.contains("directory-item")) {
+      const employeeEmail = e.target.querySelector("p.email").innerHTML;
+      const employee = findEmployeeByEmail(employeesJSON, employeeEmail);
+
+      let divHTML = "<div class='modal'>";
+      divHTML += `<img src='${employee.picture.large}'`;
+      divHTML += `alt='Picture of ${employee.name.first} ${employee.name.last}'>`;
+      divHTML += `<p class='name'>${employee.name.first} ${employee.name.last}</p>`;
+      divHTML += `<p class='email'>${employee.email}</p>`;
+      divHTML += `<p class='city-location'>${employee.location.city}</p>`;
+      divHTML += `<p class='cell-number'>${employee.cell}</p>`;
+      divHTML += `<p class='address'>${employee.location.street.number} `;
+      divHTML += `${employee.location.street.name}, ${employee.location.city}`;
+      divHTML += `, ${employee.location.state} ${employee.location.postcode}</p>`;
+      divHTML += `<p class='birthday'>Birthday: `;
+      divHTML += `${moment(employee.dob.date).format("MM/DD/YY")}</p>`;
+      divHTML += `<button class="close"><svg><use href="#close-icon"></use></svg></button></div>`;
+
+      const lightbox = document.createElement("DIV");
+      lightbox.classList.add("lightbox");
+      lightbox.innerHTML = divHTML;
+      lightbox.addEventListener("click", (e) => {
+        if (e.target.classList.contains("close")) {
+          e.target.parentNode.parentNode.parentNode.removeChild(e.target.parentNode.parentNode);
+        }
+      });
+      body.appendChild(lightbox);
+    }
+  }
+
+  let employeesJSON = [];
+
+  /* ============================================= */
+  /*                  DOM selectors                */
+  /* ============================================= */
+
+  const body = document.querySelector("body");
+  const directoryList = document.querySelector("ul.directory-list");
+
+  /* ============================================= */
+  /*                  Event handlers               */
+  /* ============================================= */
+
+  // on DOMContentLoaded
   fetchJSONfrom("https://randomuser.me/api/?results=12")
     .then(displayDirectory)
+    .then(saveJSON)
     .catch(reason => console.log("Error: HTML can't be generated"));
+
+  directoryList.addEventListener("click", createModal);
 });
